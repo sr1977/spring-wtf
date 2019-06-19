f70cc790a240e4a0fed3e8a23ffcc1e3a62a6e0f
Steve Risdon <sri01@sbgml04219.sbg.skybettingandgaming.com>
2019-06-19 10:35:12 +0100

    Added parameter

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # spring-wtf
+
+
+curl -sS -X POST http://localhost:8080/ -F body=@./test.txt
--- src/main/java/com/example/demo/DemoController.java
+++ src/main/java/com/example/demo/DemoController.java
@@ -6,11 +6,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class DemoController {
     @RequestMapping(value = "", method = RequestMethod.POST)
-    public ResponseEntity<String> pong(@RequestBody String body) {
-        return new ResponseEntity<>("FINISHED: " + body.length(), HttpStatus.OK);
+    public ResponseEntity<String> pong(@RequestParam(value = "body") MultipartFile body) {
+        return new ResponseEntity<>("FINISHED: " + body.getSize(), HttpStatus.OK);
     }
 }
